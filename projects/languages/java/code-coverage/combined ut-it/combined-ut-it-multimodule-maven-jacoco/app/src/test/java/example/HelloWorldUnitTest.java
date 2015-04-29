cc19b05b21fb9cb4d39ad0b3a6abc456f0453777
kim.moses@cashslide.co.kr <creatinov.kim@gmail.com>
2015-04-29 12:33:45 +0900

    initial commit

--- projects/languages/java/code-coverage/combined ut-it/combined-ut-it-multimodule-maven-jacoco/app/src/test/java/example/HelloWorldUnitTest.java
+++ projects/languages/java/code-coverage/combined ut-it/combined-ut-it-multimodule-maven-jacoco/app/src/test/java/example/HelloWorldUnitTest.java
@@ -3,10 +3,10 @@ package example;
 import org.junit.Test;
 
 public class HelloWorldUnitTest {
-    
-	@Test
-    public void test() {
-        new HelloWorld().coveredByUnitTest();
-    }
-	
+
+	//@Test
+    //public void test() {
+    //    new HelloWorld().coveredByUnitTest();
+    //}
+
 }
